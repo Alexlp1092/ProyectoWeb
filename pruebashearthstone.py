1553be5f0e8c8c25415e7b6e9ab8a83a3ae25fe7
Alejandro López Peña <36158063+Alexlp1092@users.noreply.github.com>
2018-05-15 21:18:10 +0200

    Pruebas

---
+++ pruebashearthstone.py
@@ -0,0 +1,41 @@
+import json
+from pprint import pprint
+import requests
+
+print("¿Que quieres?")
+print("Buscar una carta")
+print("Buscar varias")
+opcion=int(input("¿Que quieres?"))
+
+if opcion==1:
+  carta=input("Introduzca el nombre")
+  response = requests.get("https://omgvamp-hearthstone-v1.p.mashape.com/cards/"+carta,
+  headers={
+    "X-Mashape-Key": "RdEHCET0tBmshzcVxojLE997hAvNp1qWbaQjsn2UdJz0ad4JQA",
+    "Accept": "application/json"
+  }
+)
+  data=response.json()
+  for carta in data:
+    if carta["collectible"]==True:
+      print("Nombre:",carta["name"])
+      print("Vida:",carta["health"])
+      print("Ataque:",carta["attack"])
+
+else:
+  print("Valor gili")
+
+
+
+expansion="¿Que expansion deseas buscar?"
+
+response = requests.get("https://omgvamp-hearthstone-v1.p.mashape.com/cards/sets/"+expansion,
+  headers={
+    "X-Mashape-Key": "RdEHCET0tBmshzcVxojLE997hAvNp1qWbaQjsn2UdJz0ad4JQA"
+  }
+)
+data=response.json()
+
+for carta in data:
+	if carta["type"]=="Hero":
+		print (carta["name"])
