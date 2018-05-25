fdac82ca39aeae3bbc730866ba932d077a8932f8
Alejandro López Peña <36158063+Alexlp1092@users.noreply.github.com>
2018-05-25 12:29:28 +0200

    Update funcion1.py

--- funcion1.py
+++ funcion1.py
@@ -1,6 +1,8 @@
 import json
 from pprint import pprint
 import requests
+from colorama import Back, Style, Fore, init
+init(autoreset=True)
 
 expansion=input("Introduzca nombre de la carta: ")
 
@@ -14,10 +16,45 @@ nombre=input("Introduzca nombre de la carta: ")
 
 for carta in data:
 	if 'collectible' in carta and carta["name"]==nombre:
-		print("Nombre:", carta["name"])
-		print("Tipo:", carta["type"])
-		print("Rareza:", carta["rarity"])
-		print("Coste:", carta["cost"])
-		print("Ataque:", carta["attack"])
-		print("Vida:", carta["health"])
-		print("Descripcion:", carta["text"])
+		print(Style.BRIGHT+"Nombre:", carta["name"])
+
+		print(Style.BRIGHT+"Tipo:", carta["type"])
+
+		if carta["rarity"]=="Common":
+			print(Style.BRIGHT+"Rareza:", carta["rarity"])
+		elif carta["rarity"]=="Rare":
+			print(Style.BRIGHT+"Rareza:",Fore.BLUE+carta["rarity"])
+		elif carta["rarity"]=="Epic":
+			print(Style.BRIGHT+"Rareza:",Fore.MAGENTA+carta["rarity"])		
+		elif carta["rarity"]=="Legendary":
+			print(Style.BRIGHT+"Rareza:",Fore.YELLOW+carta["rarity"])	
+
+		print(Style.BRIGHT+"Coste:",Fore.CYAN+str(carta["cost"]))
+
+		if 'attack' in carta:
+			print(Style.BRIGHT+"Ataque:",Fore.YELLOW+str(carta["attack"]))
+
+		if 'health' in carta:
+			print(Style.BRIGHT+"Vida:",Fore.RED+str(carta["health"]))
+
+		if carta["playerClass"]=="Warrior":
+			print(Style.BRIGHT+"Clase:",Fore.RED+carta["playerClass"])
+		elif carta["playerClass"]=="Shaman":
+			print(Style.BRIGHT+"Clase:",Fore.BLUE+carta["playerClass"])
+		elif carta["playerClass"]=="Rogue":
+			print(Style.BRIGHT+"Clase:",Fore.BLACK+carta["playerClass"])
+		elif carta["playerClass"]=="Paladin":
+			print(Style.BRIGHT+"Clase:",Fore.YELLOW+carta["playerClass"])
+		elif carta["playerClass"]=="Hunter":
+			print(Style.BRIGHT+"Clase:",Fore.GREEN+carta["playerClass"])
+		elif carta["playerClass"]=="Druid":
+			print(Style.BRIGHT+"Clase:",Fore.BROWN+carta["playerClass"])
+		elif carta["playerClass"]=="Warlock":
+			print(Style.BRIGHT+"Clase:",Fore.MAGENTA+carta["playerClass"])
+		elif carta["playerClass"]=="Mage":
+			print(Style.BRIGHT+"Clase:",Fore.CYAN+carta["playerClass"])
+		else:
+			print(Style.BRIGHT+"Clase:",carta["playerClass"])
+
+		if 'text' in carta:
+			print(Style.BRIGHT+"Descripcion:", carta["text"])
