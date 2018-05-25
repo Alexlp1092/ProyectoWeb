fe7e86abbe7bee496952621ccf6096e48eff43b0
Alejandro López Peña <36158063+Alexlp1092@users.noreply.github.com>
2018-05-25 12:29:58 +0200

    Update funcion2.py

--- funcion2.py
+++ funcion2.py
@@ -1,6 +1,8 @@
 import json
 from pprint import pprint
 import requests
+from colorama import Back, Style, Fore, init
+init(autoreset=True)
 
 expansion=input("Introduzca nombre de la carta: ")
 
@@ -13,6 +15,23 @@ contador=0
 data=response.json()
 for carta in data:
 	if 'rarity' in carta and carta["rarity"]=="Legendary" and 'collectible' in carta:
-		print(carta["name"])
+		if carta["playerClass"]=="Warrior":
+			print(Fore.RED+carta["name"])
+		elif carta["playerClass"]=="Shaman":
+			print(Fore.BLUE+carta["name"])
+		elif carta["playerClass"]=="Rogue":
+			print(Fore.BLACK+carta["name"])
+		elif carta["playerClass"]=="Paladin":
+			print(Fore.YELLOW+carta["name"])
+		elif carta["playerClass"]=="Hunter":
+			print(Fore.GREEN+carta["name"])
+		elif carta["playerClass"]=="Druid":
+			print("\033[32;33;40m"+carta["name"])
+		elif carta["playerClass"]=="Warlock":
+			print(Fore.MAGENTA+carta["name"])
+		elif carta["playerClass"]=="Mage":
+			print(Fore.CYAN+carta["name"])
+		else:
+			print(carta["name"])
 		contador=contador+1
 print("En esta expansion hay ",contador,"cartas legendarias")
